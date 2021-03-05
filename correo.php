4804396070a3ff63911b76c0b62a53da70ebab57
TreschiOrginal <71459781+TreschiOrginal@users.noreply.github.com>
2021-03-04 18:04:05 -0600

    Add files via upload

--- correo.php
+++ correo.php
@@ -5,10 +5,6 @@
      $name = $_POST[name];
      $asunto = $_POST[tel];
      $msg = $_POST[mensj];
-     $header = "From:" . "/r/n";
-     $headre.= "Reply-to:" . "/r/n";
-     $header.= "X-Mailer: PHP/". phpversion();
-     mail()
   if ($mail) {
     echo "<h4>¡Mail enviado exitosamente!</h4>";
   }
